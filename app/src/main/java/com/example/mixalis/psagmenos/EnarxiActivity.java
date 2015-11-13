3a9c38fd3684f4eda1fea2f4081f5e3611245bbc
mixalis <mvamvolis@powercontrol.gr>
2015-11-13 23:39:58 +0200

    eftiaksa keno

--- app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
@@ -1,13 +1,38 @@
 package com.example.mixalis.psagmenos;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import static android.media.MediaPlayer.create;
 
 /**
  * Created by mixalis on 13/11/2015.
  */
 public class EnarxiActivity extends Activity {
+    ListView listView;
+    String[] katigories = new String[]{
+            "Γεωγραφία",
+            "Ιστορία",
+            "Αθλητικά",
+            "Τεχνολογία",
+    };
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enarxi);
+
+        CustomAdapter adapter = new CustomAdapter(this, katigories);
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+    }
 
 }
--- app/src/main/java/com/example/mixalis/psagmenos/MainActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/MainActivity.java
@@ -1,25 +1,76 @@
 package com.example.mixalis.psagmenos;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import static android.media.MediaPlayer.*;
+import static com.example.mixalis.psagmenos.R.*;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView enarxi;
+    MediaPlayer mediaPlayer;
 
+
+
+
+
+
+    @Override
+    public void onBackPressed()
+    {
+        // super.onBackPressed();
+        mediaPlayer.stop();
+        finish();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+      mediaPlayer = create(this, raw.back1);
+        mediaPlayer.start();
+
+
+        enarxi = (TextView) findViewById(id.enarxi);
+        enarxi.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+
+                Intent myIntent = new Intent(MainActivity.this, EnarxiActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+
+        });
 
     }
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
+
     }
+
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
