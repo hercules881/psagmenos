2311ffbcb3324c363d1227568e4c9a813ece4276
mixalis <mvamvolis@powercontrol.gr>
2015-11-13 21:37:55 +0200

    eftiaksa keno

--- app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/EnarxiActivity.java
@@ -1,9 +1,13 @@
 package com.example.mixalis.psagmenos;
 
 import android.app.Activity;
+import android.util.Log;
 
 /**
  * Created by mixalis on 13/11/2015.
  */
 public class EnarxiActivity extends Activity {
+
+
+
 }
