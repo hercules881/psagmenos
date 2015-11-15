393969945ca19b051a4ea8e0aaf33df0f914b9d5
mixalis <mvamvolis@powercontrol.gr>
2015-11-15 12:52:08 +0200

    δεσ τοο

--- app/src/main/java/com/example/mixalis/psagmenos/GameActivity.java
+++ app/src/main/java/com/example/mixalis/psagmenos/GameActivity.java
@@ -54,6 +54,8 @@ public class GameActivity extends Activity {
         erwtisi.setText(questions.get(0).getText());
 
 
+
+
       ArrayList<Answer>answers=(ArrayList<Answer>) dbHelper.getPossibleAnswersForQuestion(questions.get(0));//apantiseis
         answers.get(0);
         apantisi1.setText(answers.get(0).getText());
